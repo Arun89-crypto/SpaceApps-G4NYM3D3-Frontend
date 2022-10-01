7826904670b2e638a3234629374a5d663c57940d
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-10-01 12:59:57 +0530

    Setup Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-  <></>
+  <Route path="/home" exact component={HomePage} />
   );
 }
 
