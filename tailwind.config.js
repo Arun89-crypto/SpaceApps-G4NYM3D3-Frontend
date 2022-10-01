b382ca9ac94c34d8a0d8cd918fa94db54044a399
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-10-02 01:15:02 +0530

    Created Chat FE template

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ module.exports = {
         acc_gray: "#3d3741",
         nav_bg:"#23192a",
         log_bt_br:"#B18FE3",
-        log_bt_bg:"#5A4B74"
+        log_bt_bg:"#5A4B74",
+        chat_l_bg:"#403A43",
+        mes_bg:"#2A252C",
+        chat_bg:"#1C1026"
       },
     },
   },
